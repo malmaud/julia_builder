432f6427eb138725632e55fab152bc7b5ef97a2f
Jon Malmaud <malmaud@gmail.com>
2017-03-09 13:23:00 -0500

    Container Build Engine scripts

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+zip -j julia_build.zip julia-docker/julia/*
+gsutil cp julia_build.zip gs://$JL_BUCKET_NAME/julia_build.zip
+rm julia_build.zip
+gcloud container builds submit --config cloudbuild.yaml gs://$JL_BUCKET_NAME/julia_build.zip
