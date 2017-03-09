6f65bfaac7d5bb6be783b99840d3acc47bbf3c8e
Jon Malmaud <malmaud@gmail.com>
2017-03-09 13:33:35 -0500

    Update timeout

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 zip -j julia_build.zip julia-docker/julia/*
 gsutil cp julia_build.zip gs://$JL_BUCKET_NAME/julia_build.zip
 rm julia_build.zip
-gcloud container builds submit --config cloudbuild.yaml gs://$JL_BUCKET_NAME/julia_build.zip
+gcloud container builds submit --config cloudbuild.yaml --timeout 3h gs://$JL_BUCKET_NAME/julia_build.zip
